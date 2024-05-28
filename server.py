8d0acc4e2ccedd66e3425b82c4c73a534511fd17
andreiDraghiceanu <andrei.draghiceanu@gmail.com>
2024-05-28 14:40:36 +0300

    Updated requirements file. Also form update for server.py

--- server.py
+++ server.py
@@ -4,26 +4,23 @@ import csv
 
 app = Flask(__name__)
 Analytics(app)
-app.config['ANALYTICS']['GAUGES']['SITE_ID'] = '234599346'
+app.config['ANALYTICS'] = {'GAUGES': {'SITE_ID': '234599346'}}
 
 @app.route('/')
 def my_home():
-    return render_template('/index.html')
-
+    return render_template('index.html')
 
 @app.route('/<string:page_name>')
 def index(page_name):
     return render_template(page_name)
 
-
 def write_to_csv(data):
-    with open('database.csv', newline='', mode='a') as database:
+    with open('database.csv', mode='a', newline='') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
-
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -33,6 +30,11 @@ def submit_form():
             write_to_csv(data)
             return redirect('/thankyou.html')
         except Exception as err:
-            return 'did not save to database \n', err
+            # Log the error here if needed
+            print(f"Error: {err}")
+            return 'Did not save to database. Please try again later.'
     else:
-        return 'something went wrong. Try again!'
+        return 'Something went wrong. Please try again.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
